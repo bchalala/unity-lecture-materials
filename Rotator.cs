20a999882364d8f0c11530dd2ba994708d264124
Anthony Mirand <anthonypmirand@gmail.com>
2016-04-14 16:50:29 -0700

    Make cube spinning parameters public for student manipulation

--- Rotator.cs
+++ Rotator.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
+    public int x_speed;
+    public int y_speed;
+    public int z_speed;
+    
     void Update () 
     {
-        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+        transform.Rotate (new Vector3 (x_speed, y_speed, z_speed) * Time.deltaTime);
     }
 }
